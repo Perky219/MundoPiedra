18041c43533cd0d83146440199f09211dffa7d11
JosueMatamoros <1002matamoros@gmail.com>
2025-10-01 22:09:13 -0600

    feat: Implement shooting mechanics with left mouse click

--- Assets/Content/Models/MainCharacter/Scripts/ThirdPersonController.cs
+++ Assets/Content/Models/MainCharacter/Scripts/ThirdPersonController.cs
@@ -18,57 +18,26 @@ namespace StarterAssets
         [Header("Gravity")]
         public float Gravity = -15.0f;
 
-        [Header("Camera")]
-        public GameObject CinemachineCameraTarget;
-        public float TopClamp = 70.0f;
-        public float BottomClamp = -30.0f;
-        public float CameraAngleOverride = 0.0f;
-        public bool LockCameraPosition = false;
+        [Header("Rotation")]
+        public float RotationSensitivity = 5f;  // sensibilidad de rotación con el mouse
 
-        // internal
-        private float _cinemachineTargetYaw;
-        private float _cinemachineTargetPitch;
         private float _verticalVelocity;
         private float _terminalVelocity = 53.0f;
 
-        // animator parameters
+        private Animator _animator;
+        private CharacterController _controller;
+        private StarterAssetsInputs _input;
+        private bool _hasAnimator;
+
         private int _animIDHorizontal;
         private int _animIDVertical;
 
 #if ENABLE_INPUT_SYSTEM 
         private PlayerInput _playerInput;
 #endif
-        private Animator _animator;
-        private CharacterController _controller;
-        private StarterAssetsInputs _input;
-        private GameObject _mainCamera;
-        private bool _hasAnimator;
-
-        private const float _threshold = 0.01f;
-
-        private bool IsCurrentDeviceMouse
-        {
-            get
-            {
-#if ENABLE_INPUT_SYSTEM
-                return _playerInput.currentControlScheme == "KeyboardMouse";
-#else
-                return false;
-#endif
-            }
-        }
-
-        private void Awake()
-        {
-            if (_mainCamera == null)
-            {
-                _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
-            }
-        }
 
         private void Start()
         {
-            _cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
             _hasAnimator = TryGetComponent(out _animator);
             _controller = GetComponent<CharacterController>();
             _input = GetComponent<StarterAssetsInputs>();
@@ -82,12 +51,8 @@ namespace StarterAssets
         {
             _hasAnimator = TryGetComponent(out _animator);
             ApplyGravity();
+            RotateWithMouse();
             Move();
-        }
-
-        private void LateUpdate()
-        {
-            CameraRotation();
         }
 
         private void AssignAnimationIDs()
@@ -96,22 +61,11 @@ namespace StarterAssets
             _animIDVertical = Animator.StringToHash("Vertical");
         }
 
-        private void CameraRotation()
+        private void RotateWithMouse()
         {
-            if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
-            {
-                float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
-
-                _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier;
-                _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier;
-            }
-
-            _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
-            _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
-
-            CinemachineCameraTarget.transform.rotation = Quaternion.Euler(
-                _cinemachineTargetPitch + CameraAngleOverride,
-                _cinemachineTargetYaw, 0.0f);
+            // rotar al personaje con el mouse en el eje Y
+            float mouseX = _input.look.x;
+            transform.Rotate(Vector3.up * mouseX * RotationSensitivity);
         }
 
         private void Move()
@@ -119,6 +73,7 @@ namespace StarterAssets
             float targetSpeed = _input.sprint ? SprintSpeed : MoveSpeed;
             if (_input.move == Vector2.zero) targetSpeed = 0.0f;
 
+            // WASD siempre relativo al personaje
             Vector3 move = transform.right * _input.move.x + transform.forward * _input.move.y;
 
             _controller.Move(move.normalized * (targetSpeed * Time.deltaTime) +
@@ -126,12 +81,8 @@ namespace StarterAssets
 
             if (_hasAnimator)
             {
-                // Dead zone: evita que se envíen valores muy pequeños al Animator
-                float h = Mathf.Abs(_input.move.x) < 0.1f ? 0f : _input.move.x;
-                float v = Mathf.Abs(_input.move.y) < 0.1f ? 0f : _input.move.y;
-
-                _animator.SetFloat(_animIDHorizontal, h);
-                _animator.SetFloat(_animIDVertical, v);
+                _animator.SetFloat(_animIDHorizontal, _input.move.x);
+                _animator.SetFloat(_animIDVertical, _input.move.y);
             }
         }
 
@@ -141,13 +92,6 @@ namespace StarterAssets
             {
                 _verticalVelocity += Gravity * Time.deltaTime;
             }
-        }
-
-        private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
-        {
-            if (lfAngle < -360f) lfAngle += 360f;
-            if (lfAngle > 360f) lfAngle -= 360f;
-            return Mathf.Clamp(lfAngle, lfMin, lfMax);
         }
     }
 }
