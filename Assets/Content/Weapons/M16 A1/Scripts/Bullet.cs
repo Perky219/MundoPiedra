cb020eded6818be16c6f201bb06443a3f7a4547f
JosueMatamoros <1002matamoros@gmail.com>
2025-11-10 21:40:42 -0600

    feat: Increase bullet damage in Bullet.cs and add PlayerStats script

--- Assets/Content/Weapons/M16 A1/Scripts/Bullet.cs
+++ Assets/Content/Weapons/M16 A1/Scripts/Bullet.cs
@@ -2,21 +2,30 @@ using UnityEngine;
 
 public class SimpleBullet : MonoBehaviour
 {
+    public float baseDamage = 10f;
     public float speed = 40f;
     public float lifeTime = 5f;
     public GameObject destroyEffect;
 
     private Rigidbody rb;
 
-    private void Start()
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.velocity = transform.forward * speed;
         Destroy(gameObject, lifeTime);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
+        float finalDamage = baseDamage * PlayerStats.Instance.damageMultiplier;
+
+        Health targetHealth = collision.gameObject.GetComponent<Health>();
+        if (targetHealth != null)
+        {
+            targetHealth.TakeDamage(Mathf.RoundToInt(finalDamage));
+        }
+
         if (destroyEffect != null)
         {
             Instantiate(destroyEffect, transform.position, Quaternion.identity);
